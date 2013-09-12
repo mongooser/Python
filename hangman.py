29c768b42b4850755498dba06ee49ce9cd5bd4b7
mongooser <mongooser@hushmail.com>
2013-09-12 16:02:40 -0500

    Fixed ASCII Art

---
+++ hangman.py
@@ -0,0 +1,77 @@
+#! usr/bin/var
+
+import random
+HANGMAPIC = ['''
+
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose
+'mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.spilt()
+
+def getRandomWord(wordList):
+    #this function returns a random string from the list of strings
+    wordIndex = random.randint(0, len(wordList) - 1)
+    return wordList[wordIndex]
+
+def displayBoard(HANGMANPICS, missedLetters, correctLetters, secretWord):
+    print(HANGMANPIC[len(missedLetters)])
+    print()
+
+ 
+  
+
+      
---
+++ hello.py
@@ -0,0 +1,6 @@
+#This program says hello and ask for name
+
+print("Hello world!")
+print("What is your name?")
+myname = input('>')
+print("Hello, " + myname , " nice to meet you")
